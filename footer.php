557e41b2507ad1ae98cec0ef8ae5d862c061bace
triasilviana <63859580+triasilviana@users.noreply.github.com>
2020-04-25 09:12:17 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<script src="http://localhost/corona/aset/jquery.js"></script>
+    <script src="http://localhost/corona/aset/bootstrap/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
