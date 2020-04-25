3f03cc23a45b59c39ae82b1f9d11c9479be69b0f
triasilviana <63859580+triasilviana@users.noreply.github.com>
2020-04-25 08:41:56 +0700

    Add files via upload

---
+++ about/index.php
@@ -0,0 +1,39 @@
+<?php
+
+$koneksi= mysqli_connect("localhost","root","","corona");
+include '../aset/header.php';
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img src="39.jpg" width="200" class="rounded-circle">
+          <h1 class="display-4">Tria Silviana</h1>
+          <p class="lead">Selamat Datang di Website Saya</p>
+          <p class="lead">Saya dari kelas XRPL7/39</p>
+          <p class="lead">SMK TELKOM MALANG</p>
+          <p class="lead">Kali ini, saya membuat website mengenai</p>
+          <p class="lead">virus corona.</p>
+        </div>
+      </div>
+</body>
+</html>
+
+
+<?php
+include '../aset/footer.php';
+?>
